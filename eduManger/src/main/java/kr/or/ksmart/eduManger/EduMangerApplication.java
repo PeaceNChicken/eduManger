7dc94724f531a8de25a456e12aa3f23787811942
PeaceNChicken <kkjlevel33@gmail.com>
2019-09-11 16:01:43 +0900

    32기 3팀 프로젝트 시작

---
+++ eduManger/src/main/java/kr/or/ksmart/eduManger/EduMangerApplication.java
@@ -0,0 +1,13 @@
+package kr.or.ksmart.eduManger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EduMangerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EduMangerApplication.class, args);
+	}
+
+}
