e75e73b205859efbf8bb2676528eb5b701219631
seoyulLee <gorgeouslee0917@gmail.com>
2019-09-23 17:02:19 +0900

    profesCareerList.html << grid 로 나누었는데 왜 표가 밖으로 튀어나오는지 모르겠습니다..

--- eduManger/src/main/java/com/cafe24/eduManger/ProfesCarController/ProfessorCareerController.java
+++ eduManger/src/main/java/com/cafe24/eduManger/ProfesCarController/ProfessorCareerController.java
@@ -1,5 +1,16 @@
 package com.cafe24.eduManger.ProfesCarController;
 
-public class TestController {
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
-}
+@Controller
+public class ProfessorCareerController {
+
+		@GetMapping("profesCareerList")
+		public String professorCareerSelect() {
+			return "profesCar/profesCareerList";
+		}
+		
+	}
+
+
--- eduManger/src/main/java/com/cafe24/eduManger/StudentController/StuCounselController.java
+++ eduManger/src/main/java/com/cafe24/eduManger/StudentController/StuCounselController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class StuCounselController {
 
 	@GetMapping("stuCounsel")
-	public String stuSangdam() {
+	public String stuCounselSelect() {
 		return "/students/stuCounsel/stuCounselList";
 	}
 }
