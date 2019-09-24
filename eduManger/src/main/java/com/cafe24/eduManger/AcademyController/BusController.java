4d603d93270754058c2d13cf02e7f8f1b32071b8
jjdev <gmlwhd8848@naver.com>
2019-09-24 14:41:54 +0900

    AcademyController패키지에 boardcontroller,buscontroller,buslinecontroller,BusoutController,BustimeController,DriveController 클래스 만들었고
    busInsert.html(차량 등록화면 완성함)

---
+++ eduManger/src/main/java/com/cafe24/eduManger/AcademyController/BusController.java
@@ -0,0 +1,19 @@
+package com.cafe24.eduManger.AcademyController;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class BusController {
+
+	@GetMapping("/busList")
+	public String getBusList() {
+		return "/academy/bus/busList";
+	}
+	
+	@GetMapping("/busInsert")
+	public String getBusInsert() {
+		return "/academy/bus/busInsert";
+	}
+}
