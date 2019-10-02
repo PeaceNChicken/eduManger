e2cf08c178a213088e8fef54e2a97223b4fcfcb4
seoyulLee <gorgeouslee0917@gmail.com>
2019-10-02 16:27:47 +0900

    학년 및 수업과정 분류 1. 수업과정 추가하기 버튼 오른쪽으로 이됭 2. insert페이지 만들기 3. 수정버튼 링크추가 3-1. update 페이지 만들기 4. delete링크 추가 4-1. delete 페이지 만들기 5. db에 리스트 insert하기

--- eduManger/src/main/java/com/cafe24/eduManger/LectureController/ClassLevelController.java
+++ eduManger/src/main/java/com/cafe24/eduManger/LectureController/ClassLevelController.java
@@ -5,17 +5,31 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class ClassLevelController {
-
+	//학년 및 수업과정 분류 리스트
 	@GetMapping("/getclassLevel")
 		public String classLevelSelect() {
 			return "/lecture/classLevel/classLevelList";
 		}
 	
-	
+	//학년 및 수업과정 분류 추가
 	@GetMapping("/classLevelInsert")
 		public String classLevelInsert() {
 			return "/lecture/classLevel/classLevelInsert";
 	}
+	
+	//학년 및 수업과정 분류 수정
+	@GetMapping("/classLevelUpdate")
+		public String classLevelUpdate() {
+			return "/lecture/classLevel/classLevelUpdate";
+	}
+	
+	//학년 및 수업과정 분류 삭제
+	@GetMapping("/classLevelDelete")
+		public String classLevelDelete() {
+			return "/lecture/classLevel/classLevelDelete";
+	}
+	
+	
 	
 	
 }
