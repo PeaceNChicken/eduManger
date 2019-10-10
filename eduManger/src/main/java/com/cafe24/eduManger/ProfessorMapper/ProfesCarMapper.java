28cf1e3879a375e6ec349f6e444b49174d7d01ce
seoyulLee <gorgeouslee0917@gmail.com>
2019-10-10 17:04:36 +0900

    현재개설된 과목 insert화면 정리 -권한별 설정 필요해서 일단은 에러가 나는게 맞습니다..!

--- eduManger/src/main/java/com/cafe24/eduManger/ProfessorMapper/ProfesCarMapper.java
+++ eduManger/src/main/java/com/cafe24/eduManger/ProfessorMapper/ProfesCarMapper.java
@@ -2,8 +2,11 @@ package com.cafe24.eduManger.ProfessorMapper;
 
 import java.util.List;
 
+import org.apache.ibatis.annotations.Mapper;
+
 import com.cafe24.eduManger.ProfessorVo.ProfesCar;
 
+@Mapper
 public interface ProfesCarMapper {
 
 	public List<ProfesCar> ProfesCarList();
